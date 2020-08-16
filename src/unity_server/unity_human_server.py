79321e84ba636d0994e5dcfb735dfb6e731c2a5f
Mingfei Sun <mingfei.sun@ust.hk>
2020-08-16 23:25:32 +0100

    add server

--- src/unity_server/unity_human_server.py
+++ src/unity_server/unity_human_server.py
@@ -1,124 +1,55 @@
 import os
+import tf
 import ast
 import gym
+import math
+import rospy
 import socket
+import string
 import numpy as np
 from gym import spaces, utils
+from collections import deque
+from bvh_broadcaster import BVHBroadcaster
 
-BONE_MAPPING = {}
-
-def decode_recv(_recv):
-    state_strs = _recv.split('#')[1:-1] # start and end with #
-    state_list = []
-    for each in state_strs:
-        state_list.append(np.fromstring(each[1:-1], dtype=np.float, sep=','))
-
-    return np.array(state_list)
-
-def encode_send(_to_send, _num_frames, prefix):
-    # bone name; position (3 DoF); rotation (4 DoF);
+def encode_data(frame_name, trans, rot):
+    # (bone name, position 3 DoF, rotation 4 DoF)
     # each bone has 7 arguments
-    assert len(_to_send)%3 == 0
-    assert type(prefix) == str
-    assert prefix.upper() in ["ACTION", "STATE"]
-    encode_str = '%s#'%(prefix.upper())
-    for i in range(len(_to_send)//7):
-        encode_str += "("
-        for k in range(7):
-            encode_str += '%.3f,'%(_to_send[7*i+k])
-        encode_str += "(%.3f,%.3f,%.3f)#"\
-                %(_to_send[7*i], _to_send[7*i+1], _to_send[7*i+2], _to_send[7*i+3])
-        encode_str += ')#'
-    if prefix.upper() == "ACTION":
-        encode_str += "%4d#"%(_num_frames) # add frame number to indicate action
-    else:
-        encode_str += "0000#" # add 0000 to indicate state
+    encode_str = '%s#(,'%(frame_name)
+    for i in range(3):
+        encode_str += '%.4f,'%(trans[i])
+    encode_str += ')#(,'
+    for i in range(4):
+        encode_str += '%.4f,'%(rot[i])
+    encode_str += ')'
     return encode_str
 
-class UnityHumanServer(gym.Env):
-    metadata = {"render.modes": ["human", "rgb_array"]}
-    def __init__(self):
-        HOST = "localhost"
-        PORT = 65431
+class UnityBVHSender(BVHBroadcaster):
+    def __init__(self, filename, root_frame, tcp_conn):
+        BVHReader.__init__(self, filename)
+        self.tcp_conn = tcp_conn
 
+    def sendTf(self, _trans, _rot, _time, _tf, _parent_tf):
+        self.br.sendTransform(_trans, _rot, _time, _tf, _parent_tf)
+        a_str = encode_data(_tf, _trans, _rot)
+        self.tcp_conn.sendall(str.encode(a_str))
+
+class UnityHumanServer(gym.Env):
+    def __init__(self, bvh_file='walk.bvh', root_frame='human'):
+        HOST = "localhost"
+        PORT = 65432
+        self.frame_queue = deque(maxlen=100) # frame buffer
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             s.bind((HOST, PORT))
+            print('Waiting for connection...')
             s.listen()
-            self.conn, _ = s.accept()
+            conn, _ = s.accept()
+            print('Connected!')
+        self.bvh_sender = UnityBVHSender(bvh_file, root_frame, conn)
         
-    def step(self, a, num_frames):
-        a_str = encode_send(a, num_frames, "action")
-        self.conn.sendall(str.encode(a_str))
-        ob = self._get_obs(num_frames)
-
-        reward = 0.0
-        done = False
-        meta_data = {}
-
-        return ob, reward, done, meta_data
-
-    def _get_obs(self, num_frames):
-        obs = []
-        for _ in range(num_frames):
-            ob_str = (self.conn.recv(204800)).decode()
-            ob_array = decode_recv(ob_str)
-            obs.append(ob_array)
-        return np.array(obs)
-
-    def predict(self, _states, num_frames):
-        assert num_frames == 1
-        for i in range(num_frames):
-            state = _states[i].flatten()
-            state_str = encode_send(state, num_frames, "state")
-            self.conn.sendall(str.encode(state_str))
-
-    def reset(self):
-        if not self.first_init:
-            self.conn.sendall(a_bytes)
-
-        self.first_init = False
-        return self._get_obs()
+    def send(self, loop=True):
+        self.bvh_sender.broadcast(loop=loop)
     
-    def test_a(self):
-        ob_str1 = (self.conn.recv(204800)).decode()
-        ob_str2 = (self.conn.recv(204800)).decode()
-
-        ob_array = decode_recv(ob_str1+ob_str2)
-        return ob_array
-
-    def test_b(self, _ac):
-        ac_str = encode_send(_ac, 100, "action")
-        self.conn.sendall(str.encode(ac_str))
-
-    def test_c(self, _state):
-        state = _state.flatten()
-        state_str = encode_send(state, 1, "state")
-        self.conn.sendall(str.encode(state_str))
-
 if __name__ == "__main__":
-    env = UnityClothBallEnv()
-    print("Env test")
-    # env.reset()
-    # action = np.random.rand(6)
-    # env.test_b(np.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0]))
-    import time
-
-    while True:
-        # ob, reward, done, _ = env.step([10.0, 8.0, 25.0, -25.0, 25.0, 25.0, -25.0, -25.0, -25.0, -25.0])
-        # print(ob[0], ob[1])
-        # print(done)
-        # print(reward)
-        # action = np.random.rand(6)
-        # action[3:] = action[3:] - 0.5
-        # ob = env.step(action, 500)[0]
-        # print(ob.shape)
-        # for each in ob:
-        #     env.predict(each[None], 1)
-        #     time.sleep(0.02)
-
-        # time.sleep(3)
-        ob = env.test_a()
-        print(ob.shape)
-        env.test_c(ob)
-        time.sleep(0.05)
+    env = UnityHumanServer()
+    env.send()
         
